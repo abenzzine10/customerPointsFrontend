9942e4656b334a8b25ddb29a1efbfd46c5269ca3
ayoub <ayoubbenzzine@gmail.omc>
2021-04-14 22:42:13 -0500

    Heroku deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import Purchases from './components/Purchases';
+import Points from './components/Points';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <Link to="/"><a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Points</a></Link>{' '}
+          <Link to="/purchases"><a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Purchases</a></Link>{' '} 
+          <Switch>
+            <Route exact path="/" component={Points} />
+            <Route exact path="/purchases" component={Purchases} />
+            <Route render={() => <h1>Page not found</h1>} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
